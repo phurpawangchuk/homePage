81dc9551d9c53201662776830c924cbd4b1934ad
phurpawangchuk <phurpawangchuk20@gmail.com>
2024-08-13 21:03:34 -0500

    Updated

---
+++ build/precache-manifest.f973cb804d04d49141a86a448e5a8a96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f1fcd2566584748e3490007a612c06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08c399af6bd2f57244bd",
+    "url": "/static/css/main.e1e4ae97.chunk.css"
+  },
+  {
+    "revision": "00125e2effdb236654f4",
+    "url": "/static/js/2.bf521656.chunk.js"
+  },
+  {
+    "revision": "08c399af6bd2f57244bd",
+    "url": "/static/js/main.d71b7558.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
